e73c2f105c9332a39caa367a7d850699c03428f1
haseong <kimhaseong@gmail.com>
2019-11-30 18:55:48 +0900

    update

---
+++ docker_run.bat
@@ -0,0 +1,1 @@
+docker run --rm -it -d -p 8888:8888  --name bioengml -v c:\bioengml:/home/bioengml haseong/bioengml:v1 jupyter lab --no-browser --allow-root --ip=0.0.0.0 --notebook-dir=/home/bioengml --NotebookApp.token=
